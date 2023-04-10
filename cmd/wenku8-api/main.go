fc5c1afd9d102d831a72364fcb700ef2de29b6a7
pierre0210 <gamelauncher0210@gmail.com>
2023-04-10 23:54:39 +0800

    feat: gin router

--- cmd/wenku8-api/main.go
+++ cmd/wenku8-api/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/pierre0210/wenku8-api/internal/util"
+)
+
+func main() {
+	port := flag.Int("p", 5000, "Port")
+	flag.Parse()
+
+	err := godotenv.Load()
+	util.ErrorHandler(err, true)
+
+	router := gin.Default()
+
+	novelRouter := router.Group("/novel")
+	novelRouter.GET("/chapter/:aid/:vol/:ch")
+	novelRouter.GET("/index/:aid")
+
+	router.Run(fmt.Sprintf("localhost:%d", *port))
+}
--- internal/util/util.go
+++ internal/util/util.go
@@ -1,1 +1,11 @@
 package util
+
+import "log"
+
+func ErrorHandler(err error, fatal bool) {
+	if fatal {
+		log.Fatalln(err.Error())
+	} else {
+		log.Println(err.Error())
+	}
+}
